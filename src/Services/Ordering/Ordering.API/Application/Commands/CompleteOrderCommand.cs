d89f376da548ec28746383250fd82329b74af2a3
Gamze Malakçı <gamze.malakci@gmail.com>
2022-08-27 15:05:06 +0300

    add CompleteOrderAsync
    add CompleteOrderAsync for dgpays

---
+++ src/Services/Ordering/Ordering.API/Application/Commands/CompleteOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.eShopOnContainers.Services.Ordering.API.Application.Commands;
+using Microsoft.eShopOnContainers.Services.Ordering.API.Application.Models;
+public class CompleteOrderCommand : IRequest<bool>
+{
+
+    [DataMember]
+    public int OrderNumber { get; private set; }
+
+    public CompleteOrderCommand(int orderNumber)
+    {
+        OrderNumber = orderNumber;
+    }
+
+}
